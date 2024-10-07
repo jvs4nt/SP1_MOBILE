2b4c3e652dcd1ee9a70d8d0e6d4b55ef51786bc0
jvs4nt <joaovit342@gmail.com>
2024-10-07 19:34:59 -0300

    ajuste no hint dos edittexts + evento de clique no botão de entrar

--- app/src/main/java/com/example/sprint_1_evotech/LoginActivity.kt
+++ app/src/main/java/com/example/sprint_1_evotech/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.example.sprint_1_evotech
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var errorTextView: TextView
+    private lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,19 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        errorTextView = findViewById<TextView>(R.id.errorTextView)
+        errorTextView.isVisible = false
+        loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener() {
+            login()
+        }
+
+    }
+
+    private fun login() {
+        errorTextView.text = getString(R.string.usuario_nao_encontrado)
+        errorTextView.isVisible = true
     }
 }
