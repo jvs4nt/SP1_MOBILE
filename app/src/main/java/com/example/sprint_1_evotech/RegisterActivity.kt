024f0387ae8cad53223229663bfe2d8352861dee
jvs4nt <joaovit342@gmail.com>
2024-11-08 13:38:00 -0300

    backend da activity de registro

--- app/src/main/java/com/example/sprint_1_evotech/RegisterActivity.kt
+++ app/src/main/java/com/example/sprint_1_evotech/RegisterActivity.kt
@@ -1,12 +1,25 @@
 package com.example.sprint_1_evotech
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var errorTextView: TextView
+    private lateinit var registerButton: Button
+    private lateinit var button2: Button
+    private lateinit var loginEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var nameEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +28,40 @@ class RegisterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        errorTextView = findViewById(R.id.errorTextView)
+        loginEditText = findViewById(R.id.loginEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        registerButton = findViewById(R.id.registerButton)
+        button2 = findViewById(R.id.button2)
+        nameEditText = findViewById(R.id.nameEditText)
+
+        errorTextView.isVisible = false
+
+        registerButton.setOnClickListener {
+            register()
+        }
+
+        button2.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun register() {
+        val username = loginEditText.text.toString()
+        val password = passwordEditText.text.toString()
+        val nome = nameEditText.text.toString()
+
+        if (username.isNotEmpty() && password.isNotEmpty() && nome.isNotEmpty()) {
+            errorTextView.setBackgroundColor(Color.parseColor("#96ff9d"))
+            errorTextView.text = getString(R.string.registro_sucesso)
+            errorTextView.isVisible = true
+        } else {
+            errorTextView.setBackgroundColor(Color.parseColor("#f76f7d"))
+            errorTextView.text = getString(R.string.registro_erro)
+            errorTextView.isVisible = true
         }
     }
 }
